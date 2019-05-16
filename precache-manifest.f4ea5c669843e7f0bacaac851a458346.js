a283d070ec8411a0a4cc646f332f492ce851086b
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-05-16 12:36:37 -0400

    Updates

---
+++ precache-manifest.f4ea5c669843e7f0bacaac851a458346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc5ddfa122d2af85fb3881908dd6af7",
+    "url": "/React-single-page-app/index.html"
+  },
+  {
+    "revision": "464bebbeae75d0e863f0",
+    "url": "/React-single-page-app/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "ec32125f5a3e6dc4485a",
+    "url": "/React-single-page-app/static/js/2.fb32d0ce.chunk.js"
+  },
+  {
+    "revision": "464bebbeae75d0e863f0",
+    "url": "/React-single-page-app/static/js/main.df1ec37d.chunk.js"
+  },
+  {
+    "revision": "c032b2c7a85ec8a145f0",
+    "url": "/React-single-page-app/static/js/runtime~main.797f8bc1.js"
+  }
+]);
